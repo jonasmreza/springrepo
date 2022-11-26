ac372c840c48337aa24019fae644837c4b1c565f
jonasmreza <jonasmreza@gmail.com>
2022-11-26 12:35:31 -0600

    add logic and test

---
+++ src/main/java/com/jonasmreza/jonasmreza/JonasmrezaApplication.java
@@ -0,0 +1,13 @@
+package com.jonasmreza.jonasmreza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonasmrezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonasmrezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonasmreza/jonasmreza/JonasmrezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonasmreza.jonasmreza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonasmrezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
